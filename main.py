29d0fc664f0b58bfdeb7890eb4f59fdb6ffd4e23
froas <froas.crp@gmail.com>
2025-07-05 13:44:27 +0900

    Update middleware

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://froas.github.io", 
+        "https://froas.github.io/react-openapi-sugarfree"
+    ],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET"],
     allow_headers=["*"]
 
 )
